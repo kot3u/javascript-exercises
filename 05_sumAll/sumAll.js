d9d3382b1e3cfa9c50a2cb94789b4201e8267c18
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-23 20:45:16 +0100

    Did 4 && 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+  if ((arguments[0] < 0 || arguments[1] < 0) || (typeof arguments[0] != 'number' || typeof arguments[1] != 'number')){
+    return 'ERROR';
+  }
+  else{
+    let sum = 0;
+    for (let i = Math.min.apply(null, arguments); i <= Math.max.apply(null, arguments); ++i){
+      sum += i;
+    };
+    return sum;
+  };
 };
 
 // Do not edit below this line
