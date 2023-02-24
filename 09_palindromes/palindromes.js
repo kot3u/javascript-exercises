ddfd86a11770a1d7151ca8ecab91b1a1e00d5e91
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-24 13:49:21 +0100

    did 9th one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringm = string.replace(/[^a-zA-Z]/g, "").toLowerCase()
+  if (reverseString(stringm) === stringm){
+    return true;
+  } 
 
+  return false;
+};
+
+const reverseString = function (string) {
+  let reversed = '';
+  for (i = 1; i <= string.length; ++i) {
+    reversed += string[string.length - i];
+  };
+  return reversed;
 };
 
 // Do not edit below this line
