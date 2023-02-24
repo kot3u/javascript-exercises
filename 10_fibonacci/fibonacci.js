0431eef14e55ae48571223a045930f67057f10fe
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-24 14:07:12 +0100

    did 10th one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num <= 0){
+    return 'OOPS'
+  }
+  arr = [1, 1];
+  number = parseInt(num);
+  for (let i = 2; i < num; i++) {
+    arr.push(parseInt(arr[i - 2] + arr[i - 1]));
+  };
+  return arr[num - 1];
 };
 
 // Do not edit below this line
