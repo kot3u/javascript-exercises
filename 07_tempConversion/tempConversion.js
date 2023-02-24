505c1c0fe24a85675decf4f860408cd17372e4d9
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-24 12:41:17 +0100

    did 7th one

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temprerature) {
+  let cels = (temprerature - 32) * (5 / 9);
+  return (Math.round(cels * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temprerature) {
+  let faranheit = temprerature * 9 / 5 + 32
+  return (Math.round(faranheit * 10) / 10);
 };
 
 // Do not edit below this line
