831fc48d909aa5154f9e2eb9af13c399a161b2ff
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-24 13:13:51 +0100

    did 8th one.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+  if (n === 0) {
+    return result;
+  }
+  for (let i = n; i >= 1; --i) {
+    result *= i;
+  };
+  return result
 };
 
 // Do not edit below this line
