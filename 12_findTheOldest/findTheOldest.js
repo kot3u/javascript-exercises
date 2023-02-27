0a265758c2aea5fada4f9bc142dd89dff47d85d7
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-27 16:58:21 +0100

    did 12th one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(pepole) {
+ 
+  oldest = pepole.reduce((a, b) => {
+    let aAge = getAge(a.yearOfBirth, a.yearOfDeath);
+    let bAge = getAge(b.yearOfBirth, b.yearOfDeath);
+    return aAge > bAge ? a : b;
+  })
+  return oldest;
 };
+
+const getAge = (birth, death) => {
+  if(!death){
+    death = (new Date()).getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
